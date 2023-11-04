6efbf5b49343261ae2afe2593fd7b9cb47f657f1
Yqnk <yan.bertile@gmail.com>
2023-11-04 18:31:09 +0100

    format

--- src/sudoku_solver/solver.c
+++ src/sudoku_solver/solver.c
@@ -1,218 +1,221 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
 #define N 9
- 
 
 int Sudoku_Erreur(int grid[N][N], int row,
-                       int col, int num)
+                  int col, int num)
 {
-     
     // Checking in row
     for (int x = 0; x <= 8; x++)
         if (grid[row][x] == num)
             return 0;
- 
+
     // Checking in column
     for (int x = 0; x <= 8; x++)
         if (grid[x][col] == num)
             return 0;
- 
+
     // Checking subgrid
     int startRow = row - row % 3,
-                 startCol = col - col % 3;
-    
+        startCol = col - col % 3;
+
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             if (grid[i + startRow][j +
-                          startCol] == num)
+                                   startCol] == num)
                 return 0;
- 
+
     return 1;
 }
 // Function to display the sudoku
 int solver(int grid[N][N], int row, int col)
 {
-     
-    
     if (row == N - 1 && col == N)
         return 1;
- 
-    
+
     if (col == N)
     {
         row++;
         col = 0;
     }
-   
-    
+
     if (grid[row][col] > 0)
         return solver(grid, row, col + 1);
- 
+
     for (int num = 1; num <= N; num++)
     {
-         
-        
-        if (Sudoku_Erreur(grid, row, col, num)==1)
+
+        if (Sudoku_Erreur(grid, row, col, num) == 1)
         {
-            
+
             grid[row][col] = num;
-           
-            
-            if (solver(grid, row, col + 1)==1)
+
+            if (solver(grid, row, col + 1) == 1)
                 return 1;
         }
-       
-        
+
         grid[row][col] = 0;
     }
     return 0;
 }
 
-//Main function
+// Main function
 int main(int argc, char *argv[])
 {
 
-    //Check if the input file is valid, if it's valid, read the file and store the values in a 2D array
+    // Check if the input file is valid, if it's valid, read the file and store the values in a 2D array
     FILE *inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL) {
+    if (inputFile == NULL)
+    {
         perror("Error opening input file");
         return 1;
     }
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: %s <input file>\n", argv[0]);
         return 1;
     }
-    
+
     int grid[N][N];
     int col = 0;
     int row = 0;
     char tempvalue;
-    while (!feof(inputFile)) //while not end of file keep reading the file and store the values in the 2D array if . then store 0
+    while (!feof(inputFile)) // while not end of file keep reading the file and store the values in the 2D array if . then store 0
+    {
+        char oui = fscanf(inputFile, "%c", &tempvalue);
+        oui += 1;
+
+        if (tempvalue == '1')
         {
-            char oui =fscanf(inputFile, "%c" , &tempvalue);
-            oui+=1;
-
-            if (tempvalue=='1')
-                {
-                    grid[row][col] = 1;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-
-                    
-                }
-             if (tempvalue=='2')
-                {
-                    grid[row][col] = 2;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='3')
-                {
-                    grid[row][col] = 3;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='4')
-                {
-                    grid[row][col] = 4;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='5')
-                {
-                    grid[row][col] = 5;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='6')
-                {
-                    grid[row][col] = 6;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='7')
-                {
-                    grid[row][col] = 7;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='8')
-                {
-                    grid[row][col] = 8;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='9')
-                {
-                    grid[row][col] =9;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-             if (tempvalue=='.')
-                {
-                    grid[row][col] = 0;
-                    col+=1;
-                    if (col == 9)
-                        {col = 0;
-                        row+=1;}
-                }
-            if (tempvalue==' ')
-                continue;
+            grid[row][col] = 1;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
         }
-     
+        if (tempvalue == '2')
+        {
+            grid[row][col] = 2;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '3')
+        {
+            grid[row][col] = 3;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '4')
+        {
+            grid[row][col] = 4;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '5')
+        {
+            grid[row][col] = 5;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '6')
+        {
+            grid[row][col] = 6;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '7')
+        {
+            grid[row][col] = 7;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '8')
+        {
+            grid[row][col] = 8;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '9')
+        {
+            grid[row][col] = 9;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == '.')
+        {
+            grid[row][col] = 0;
+            col += 1;
+            if (col == 9)
+            {
+                col = 0;
+                row += 1;
+            }
+        }
+        if (tempvalue == ' ')
+            continue;
+    }
+
     fclose(inputFile);
 
-            
-
-    
-    
-    
- 
-    if (solver(grid, 0, 0)==1) //if the sudoku is solved, write the solution in a file
+    if (solver(grid, 0, 0) == 1) // if the sudoku is solved, write the solution in a file
     {
         char outputFileName[strlen(argv[1]) + 8];
         strcpy(outputFileName, argv[1]);
         strcat(outputFileName, "_result");
         FILE *output;
         output = fopen(outputFileName, "w");
-        for (int i = 0; i < N; i++) //write the solution in the file
+        for (int i = 0; i < N; i++) // write the solution in the file
         {
             for (int j = 0; j < N; j++)
-                {
-                fprintf(output, "%d" ,grid[i][j]);
-                if(j==2 || j == 5 || j==8)
-                fprintf(output, " "); }
-            if(i==2 ||i == 5 || i == 8)
-                fprintf(output,"\n");
-            fprintf(output,"\n");
-
+            {
+                fprintf(output, "%d", grid[i][j]);
+                if (j == 2 || j == 5 || j == 8)
+                    fprintf(output, " ");
+            }
+            if (i == 2 || i == 5 || i == 8)
+                fprintf(output, "\n");
+            fprintf(output, "\n");
         }
         fclose(output);
-        
     }
     else
         printf("No solution exists");
- 
+
     return 0;
 }
-    
--- src/sudoku_solver/solver.h
+++ src/sudoku_solver/solver.h
@@ -1,24 +1,25 @@
 #ifndef TEST_H
 #define TEST_H
 #define N 9
-  /**
-  * @brief check if the grid doesn't contain any errors
-  * 
-  * @param grid //sudoku grid
-  * @param row  //line
-  * @param col  //column
-  * @param num //number
-  * @return int 
-  */
-int Sudoku_Erreur(int grid[N][N], int row,int col, int num);
 /**
-    * @brief this function is a solver function with backtracking
-    * 
-    * @param grid //the sudoku grid
-    * @param row 
-    * @param col 
-    * @return int 
-    */   
+ * @brief check if the grid doesn't contain any errors
+ *
+ * @param grid //sudoku grid
+ * @param row  //line
+ * @param col  //column
+ * @param num //number
+ * @return int
+ */
+int Sudoku_Erreur(int grid[N][N], int row, int col, int num);
+
+/**
+ * @brief this function is a solver function with backtracking
+ *
+ * @param grid //the sudoku grid
+ * @param row
+ * @param col
+ * @return int
+ */
 int solver(int grid[N][N], int row, int col);
 
 #endif
