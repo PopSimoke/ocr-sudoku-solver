cf0f68914a8ce3fc507462899b492773628c36f6
Lucas Stephan <lucas.stephan@epita.fr>
2023-12-02 20:01:59 +0100

    add nice wrapper to launch the ai

--- src/neural_network/number_recognition.c
+++ src/neural_network/number_recognition.c
@@ -1,16 +1,15 @@
-#include <SDL2/SDL_surface.h>
-#define INPUT_LAYER_SIZE 1024
-#define HIDDEN_LAYER_SIZE 100
-#define OUTPUT_LAYER_SIZE 9
-
+#include "number_recognition.h"
 #include "neural_network.h"
+
 #include <dirent.h>
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
 
+// SDL
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_surface.h>
 
 // Concat two string (useful for path concatenation)
 char *concat_str(char *s1, char *s2) {
@@ -20,9 +19,8 @@ char *concat_str(char *s1, char *s2) {
   return result;
 }
 
-// Convert the grid case images into exploitable data for the ai
-int compute_grid(Neural_Network *nn, char *input_path, char *output_path) {
-  char *numbers = malloc(81 * sizeof(char));
+int generate_grid(Neural_Network *nn, char *input_path, char *output_path) {
+  char numbers[81] = {0};
   memset(numbers, '.', 81);
   DIR *dir;
   struct dirent *ent;
@@ -117,38 +115,144 @@ int compute_grid(Neural_Network *nn, char *input_path, char *output_path) {
   }
 
   fclose(fptr);
-  free(numbers);
 
   return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
+int ai_wrapper_with_model(char *model_path, char *input_path,
+                          char *output_path) {
 
-  if (argc != 2) {
-    errx(1, "You have to specify the iteration count for the training.");
-  }
-
-  double learning_rate = 0.5;
-  size_t iteration_count = 0;
-  for (size_t i = 0; i < strlen(argv[1]); i++) {
-    iteration_count = iteration_count * 10 + (argv[1][i] - '0');
-  }
-
-  //    Neural_Network *nn = create_new_neural_network(learning_rate);
-
-  char *path = "./model";
   Neural_Network *nn =
-      load_neural_network(path, learning_rate, INPUT_LAYER_SIZE,
+      load_neural_network(model_path, LEARNING_RATE, INPUT_LAYER_SIZE,
                           OUTPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE);
 
-  char *input_path = "../image_processing/saved_images/";
-  char *output_path = "./output.txt";
-  compute_grid(nn, input_path, output_path);
+  int err = generate_grid(nn, input_path, output_path);
 
-  save_neural_network(nn, path);
-  printf("Model saved at : %s\n", path);
-
-  // free the allocated resources
   neural_network_destructor(nn);
+
+  return err;
+}
+
+int ai_wrapper(char *model_path, char *input_path, char *output_path) {
+
+  Neural_Network *nn = create_new_neural_network(
+      LEARNING_RATE, INPUT_LAYER_SIZE, OUTPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE);
+
+  int err = generate_grid(nn, input_path, output_path);
+
+  neural_network_destructor(nn);
+
+  return err;
+}
+
+int ai_wrapper_train(char *model_path, size_t iteration_count, char *dataset) {
+  // TODO
+}
+
+int main(int argc, char **argv) {
+
+  char *default_model_path = "./model";
+  char *default_input_path = "../image_processing/saved_images/";
+  char *default_output_path = "./output.txt";
+  char *default_dataset_path = "./dataset";
+  size_t default_iteration_count = 100;
+
+  printf("\n   ╔══════════════════════════╗\n   ║      Neural Network      "
+         "║\n   "
+         "╚══════════════════════════╝\n⑀ By Optical Crampté Recognition "
+         "⑀\n\n");
+
+  char model_path[128];
+  printf("Model Path [%s]: ", default_model_path);
+  fgets(model_path, 127, stdin);
+
+  if (model_path[0] == '\n') {
+    memmove(model_path, default_model_path, strlen(default_model_path));
+  } else
+    model_path[strlen(model_path) - 1] = 0;
+
+  char training_mode_str[128];
+  printf("Training Mode [y/N]: ");
+  fgets(training_mode_str, 127, stdin);
+
+  int training_mode =
+      training_mode_str[0] == 'y' || training_mode_str[0] == 'Y';
+
+  if (training_mode) {
+    char dataset_path[128];
+    printf("Dataset Path [%s]: ", default_dataset_path);
+    fgets(dataset_path, 127, stdin);
+
+    if (dataset_path[0] == '\n') {
+      memmove(dataset_path, default_dataset_path, strlen(default_dataset_path));
+    } else
+      dataset_path[strlen(dataset_path) - 1] = 0;
+
+    char iteration_count_str[128];
+    printf("Iteration Count [%lu]: ", default_iteration_count);
+    fgets(iteration_count_str, 127, stdin);
+
+    size_t iteration_count = 0;
+
+    if (iteration_count_str[0] == '\n') {
+      iteration_count = default_iteration_count;
+    } else {
+      iteration_count_str[strlen(iteration_count_str) - 1] = 0;
+      for (size_t i = 0; i < strlen(iteration_count_str); i++) {
+        iteration_count = iteration_count * 10 + (iteration_count_str[i] - '0');
+      }
+    }
+
+    ai_wrapper_train(model_path, iteration_count, dataset_path);
+  } else {
+    char input_path[128];
+    printf("Input Path [%s]: ", default_input_path);
+    fgets(input_path, 127, stdin);
+
+    if (input_path[0] == '\n') {
+      memmove(input_path, default_input_path, strlen(default_input_path));
+    } else
+      input_path[strlen(input_path) - 1] = 0;
+
+    char output_path[128];
+    printf("Output Path [%s]: ", default_output_path);
+    fgets(output_path, 127, stdin);
+
+    if (output_path[0] == '\n') {
+      memmove(output_path, default_output_path, strlen(default_output_path));
+    } else
+      output_path[strlen(output_path) - 1] = 0;
+
+    ai_wrapper_with_model(model_path, input_path, output_path);
+  }
+
+  //  if (argc < 2) {
+  //    errx(1, "You have to specify the iteration count for the
+  //    training.");
+  //  }
+  //
+  //  size_t iteration_count = 0;
+  //  for (size_t i = 0; i < strlen(argv[1]); i++) {
+  //    iteration_count = iteration_count * 10 + (argv[1][i] - '0');
+  //  }
+  //
+  //  //    Neural_Network *nn =
+  //  create_new_neural_network(learning_rate);
+  //
+  //  char *model_path = "./model";
+  //  printf("Loading model from: %s\n", model_path);
+  //  Neural_Network *nn =
+  //      load_neural_network(model_path, LEARNING_RATE,
+  //      INPUT_LAYER_SIZE,
+  //                          OUTPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE);
+  //
+  //  char *input_path = "../image_processing/saved_images/";
+  //  char *output_path = "./output.txt";
+  //
+  //  save_neural_network(nn, model_path);
+  //  printf("Model saved at : %s\n", model_path);
+  //
+  //  // free the allocated resources
+  //  neural_network_destructor(nn);
   return 0;
 }
---
+++ src/neural_network/number_recognition.h
@@ -0,0 +1,37 @@
+#ifndef NUMBER_RECOGNITION_H
+#define NUMBER_RECOGNITION_H
+
+#define INPUT_LAYER_SIZE 1024
+#define HIDDEN_LAYER_SIZE 100
+#define OUTPUT_LAYER_SIZE 9
+#define LEARNING_RATE 0.5
+
+#include "neural_network.h"
+
+/**
+ * The function recognize sudoku case and store the result as a text grid.
+ *
+ * @param nn Pointer to an instance of a Neural_Network struct.
+ * @param input_path Path to the file from where the function reads input neural
+ * network data.
+ * @param output_path Path to the text file of the generated sudoku grid.
+ * @return Integer indicating the success/failure of the function's execution.
+ * Typically, returns 0 upon success and non-zero values upon failure.
+ */
+int generate_grid(Neural_Network *nn, char *input_path, char *output_path);
+
+/**
+ * A wrap of all the ai methods that generates a sudoku grid from a given
+ * directory with sudoku cases inside.
+ *
+ * @param model_path Path to a model file.
+ * @param input_path Path to the directory containing the sudoku cases.
+ * @param output_path Path to the text file of the generated sudoku grid.
+ *
+ * @return Integer indicating the success/failure of the function's execution.
+ * Typically, returns 0 upon success and non-zero values upon failure.
+ */
+int ai_wrapper_with_model(char *model_path, char *input_path,
+                          char *output_path);
+
+#endif
