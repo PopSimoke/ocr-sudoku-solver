516ab3b168181acd53bc99ea481e2853e36fd9ec
Lucas Stephan <lucas.stephan@epita.fr>
2023-12-07 10:51:12 +0100

    add identity matrix generator

--- src/neural_network/matrix.c
+++ src/neural_network/matrix.c
@@ -1,135 +1,158 @@
 
 #include "matrix.h"
 #include "neural_network_utils.h"
-#include <stdlib.h>
 #include <err.h>
+#include <stdlib.h>
 
 // Create a new matrix with the given lines and columns
 Matrix *create_new_matrix(size_t line_count, size_t column_count) {
-    Matrix *matrix = malloc(sizeof(struct Matrix));
-    matrix->line_count = line_count;
-    matrix->column_count = column_count;
-    matrix->data = (double *) calloc(line_count * column_count, sizeof(double));
-    return matrix;
+  Matrix *matrix = malloc(sizeof(struct Matrix));
+  matrix->line_count = line_count;
+  matrix->column_count = column_count;
+  matrix->data = (double *)calloc(line_count * column_count, sizeof(double));
+  return matrix;
 }
 
 // Create a new matrix filled with random doubles from -1 to 1
 Matrix *create_new_random_matrix(size_t line_count, size_t column_count) {
-    Matrix *matrix = create_new_matrix(line_count, column_count);
+  Matrix *matrix = create_new_matrix(line_count, column_count);
 
-    for (size_t i = 0; i < line_count * column_count; i++) {
-        *(matrix->data + i) = generate_random_double();
-    }
+  for (size_t i = 0; i < line_count * column_count; i++) {
+    *(matrix->data + i) = generate_random_double();
+  }
 
-    return matrix;
+  return matrix;
 }
 
 // Create a new matrix filled with zeros
 Matrix *create_new_zeros_matrix(size_t line_count, size_t column_count) {
-    Matrix *matrix = create_new_matrix(line_count, column_count);
+  Matrix *matrix = create_new_matrix(line_count, column_count);
 
-    for (size_t i = 0; i < line_count * column_count; i++) {
-        *(matrix->data + i) = 0;
-    }
+  for (size_t i = 0; i < line_count * column_count; i++) {
+    *(matrix->data + i) = 0;
+  }
 
-    return matrix;
+  return matrix;
 }
 
 // Create a new column matrix from an array
 Matrix *create_new_matrix_from_array(const double list[], size_t length) {
-    Matrix *matrix = create_new_matrix(length, 1);
+  Matrix *matrix = create_new_matrix(length, 1);
 
-    for (size_t i = 0; i < length; i++)
-        *(matrix->data + i) = list[i];
+  for (size_t i = 0; i < length; i++)
+    *(matrix->data + i) = list[i];
 
-    return matrix;
+  return matrix;
 }
 
 // Get an element from a matrix by its line and column
 double *get_matrix_element(Matrix *matrix, size_t line, size_t column) {
-    return matrix->data + line * matrix->column_count + column;
+  return matrix->data + line * matrix->column_count + column;
 }
 
 // Set an element from a matrix by its line and column
-void set_matrix_element(Matrix *matrix, size_t line, size_t column, double element) {
-    *(matrix->data + line * matrix->column_count + column) = element;
+void set_matrix_element(Matrix *matrix, size_t line, size_t column,
+                        double element) {
+  *(matrix->data + line * matrix->column_count + column) = element;
 }
 
 // Dot multiply two matrix with compatible dimensions
 void matrix_dot_multiply(Matrix *m1, Matrix *m2, Matrix *result) {
 
-    if (m1->column_count != m2->line_count)
-        errx(1, "Can't dot multiply two matrix of incompatible dimension: m1(%lu,%lu) and m2(%lu,%lu)",
-             m1->line_count, m1->column_count, m2->line_count, m2->column_count);
+  if (m1->column_count != m2->line_count)
+    errx(1,
+         "Can't dot multiply two matrix of incompatible dimension: m1(%lu,%lu) "
+         "and m2(%lu,%lu)",
+         m1->line_count, m1->column_count, m2->line_count, m2->column_count);
 
-    for (size_t line = 0; line < m1->line_count; line++)
-        for (size_t column = 0; column < m2->column_count; column++) {
-            double sum = 0;
-            for (size_t i = 0; i < m1->column_count; i++)
-                sum += *get_matrix_element(m1, line, i) * *get_matrix_element(m2, i, column);
-            set_matrix_element(result, line, column, sum);
-        }
+  for (size_t line = 0; line < m1->line_count; line++)
+    for (size_t column = 0; column < m2->column_count; column++) {
+      double sum = 0;
+      for (size_t i = 0; i < m1->column_count; i++)
+        sum += *get_matrix_element(m1, line, i) *
+               *get_matrix_element(m2, i, column);
+      set_matrix_element(result, line, column, sum);
+    }
 }
 
 // Multiply two matrix with the same dimensions
 void matrix_multiply(Matrix *m1, Matrix *m2, Matrix *result) {
 
-    if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
-        errx(1, "Can't multiply two matrix of different dimensions: m1(%lu,%lu) and m2(%lu,%lu)",
-             m1->line_count, m1->column_count, m2->line_count, m2->column_count);
+  if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
+    errx(1,
+         "Can't multiply two matrix of different dimensions: m1(%lu,%lu) and "
+         "m2(%lu,%lu)",
+         m1->line_count, m1->column_count, m2->line_count, m2->column_count);
 
-    for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
-        *(result->data + i) = *(m1->data + i) * *(m2->data + i);
+  for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
+    *(result->data + i) = *(m1->data + i) * *(m2->data + i);
 }
 
 // Multiply a matrix with a double
 void matrix_multiply_by_double(Matrix *m1, double factor, Matrix *result) {
 
-    for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
-        *(result->data + i) = *(m1->data + i) * factor;
+  for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
+    *(result->data + i) = *(m1->data + i) * factor;
 }
 
 // Add two matrix with the same dimension
 void matrix_add(Matrix *m1, Matrix *m2, Matrix *result) {
 
-    if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
-        errx(1, "Can't add two matrix of different dimensions: m1(%lu,%lu) and m2(%lu,%lu)",
-             m1->line_count, m1->column_count, m2->line_count, m2->column_count);
+  if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
+    errx(1,
+         "Can't add two matrix of different dimensions: m1(%lu,%lu) and "
+         "m2(%lu,%lu)",
+         m1->line_count, m1->column_count, m2->line_count, m2->column_count);
 
-    for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
-        *(result->data + i) = *(m1->data + i) + *(m2->data + i);
+  for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
+    *(result->data + i) = *(m1->data + i) + *(m2->data + i);
 }
 
 // Subtract two matrix with the same dimension
 void matrix_subtract(Matrix *m1, Matrix *m2, Matrix *result) {
 
-    if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
-        errx(1, "Can't subtract two matrix of different dimensions: m1(%lu,%lu) and m2(%lu,%lu)",
-             m1->line_count, m1->column_count, m2->line_count, m2->column_count);
+  if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
+    errx(1,
+         "Can't subtract two matrix of different dimensions: m1(%lu,%lu) and "
+         "m2(%lu,%lu)",
+         m1->line_count, m1->column_count, m2->line_count, m2->column_count);
 
-    for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
-        *(result->data + i) = *(m1->data + i) - *(m2->data + i);
+  for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
+    *(result->data + i) = *(m1->data + i) - *(m2->data + i);
 }
 
 // Transpose matrix
 void matrix_transpose(Matrix *m, Matrix *result) {
-    for (size_t col = 0; col < m->column_count; col++)
-        for (size_t row = 0; row < m->line_count; row++)
-            set_matrix_element(result, col, row, *get_matrix_element(m, row, col));
+  for (size_t col = 0; col < m->column_count; col++)
+    for (size_t row = 0; row < m->line_count; row++)
+      set_matrix_element(result, col, row, *get_matrix_element(m, row, col));
 }
 
 // Copy m1 to m2
 void matrix_copy(Matrix *m1, Matrix *m2) {
 
-    if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
-        errx(1, "Can't copy a matrix to another one with different dimensions: m1(%lu,%lu) and m2(%lu,%lu)",
-             m1->line_count, m1->column_count, m2->line_count, m2->column_count);
+  if (m1->line_count != m2->line_count || m1->column_count != m2->column_count)
+    errx(1,
+         "Can't copy a matrix to another one with different dimensions: "
+         "m1(%lu,%lu) and m2(%lu,%lu)",
+         m1->line_count, m1->column_count, m2->line_count, m2->column_count);
 
-    for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
-        *(m2->data + i) = *(m1->data + i);
+  for (size_t i = 0; i < m1->column_count * m1->line_count; i++)
+    *(m2->data + i) = *(m1->data + i);
+}
+
+void set_identity_matrix(Matrix *m) {
+  if (m->line_count != m->column_count)
+    errx(1, "Can't set the identity matrix to an non squared matrix.");
+
+  for (size_t i = 0; i < m->line_count * m->column_count; i++)
+    m->data[i] = 0;
+
+  for (size_t i = 0; i < m->line_count; i++)
+    set_matrix_element(m, i, i, 1);
 }
 
 void matrix_destructor(Matrix *matrix) {
-    free(matrix->data);
-    free(matrix);
+  free(matrix->data);
+  free(matrix);
 }
--- src/neural_network/matrix.h
+++ src/neural_network/matrix.h
@@ -5,16 +5,16 @@
 #include <stddef.h>
 
 struct Matrix {
-    size_t line_count;
-    size_t column_count;
-    double *data;
+  size_t line_count;
+  size_t column_count;
+  double *data;
 };
 
 typedef struct Matrix Matrix;
 
 /**
  * @brief Creates a new matrix with the specified number of rows and columns.
- * 
+ *
  * @param line_count The number of rows in the matrix.
  * @param column_count The number of columns in the matrix.
  * @return A pointer to the newly allocated matrix.
@@ -22,8 +22,9 @@ typedef struct Matrix Matrix;
 Matrix *create_new_matrix(size_t line_count, size_t column_count);
 
 /**
- * @brief Creates a new matrix with the specified number of rows and columns, filled with random values.
- * 
+ * @brief Creates a new matrix with the specified number of rows and columns,
+ * filled with random values.
+ *
  * @param line_count The number of rows in the matrix.
  * @param column_count The number of columns in the matrix.
  * @return A pointer to the newly allocated matrix.
@@ -31,8 +32,9 @@ Matrix *create_new_matrix(size_t line_count, size_t column_count);
 Matrix *create_new_random_matrix(size_t line_count, size_t column_count);
 
 /**
- * @brief Creates a new matrix with the specified number of rows and columns, filled with zeros.
- * 
+ * @brief Creates a new matrix with the specified number of rows and columns,
+ * filled with zeros.
+ *
  * @param line_count The number of rows in the matrix.
  * @param column_count The number of columns in the matrix.
  * @return A pointer to the newly allocated matrix.
@@ -41,7 +43,7 @@ Matrix *create_new_zeros_matrix(size_t line_count, size_t column_count);
 
 /**
  * @brief Creates a new matrix from a specified array of values.
- * 
+ *
  * @param list The array of values to use to fill the matrix.
  * @param length The number of elements in the array.
  * @return A pointer to the newly allocated matrix.
@@ -50,7 +52,7 @@ Matrix *create_new_matrix_from_array(const double list[], size_t length);
 
 /**
  * @brief Retrieves the element from the matrix at the specified row and column.
- * 
+ *
  * @param matrix The matrix from which to extract the element.
  * @param line The row of the element.
  * @param column The column of the element.
@@ -59,18 +61,20 @@ Matrix *create_new_matrix_from_array(const double list[], size_t length);
 double *get_matrix_element(Matrix *matrix, size_t line, size_t column);
 
 /**
- * @brief Sets the element of the matrix at the specified row and column with the given value.
- * 
+ * @brief Sets the element of the matrix at the specified row and column with
+ * the given value.
+ *
  * @param matrix The matrix in which to set the element.
  * @param line The row of the element.
  * @param column The column of the element.
  * @param element The value to assign to the element.
  */
-void set_matrix_element(Matrix *matrix, size_t line, size_t column, double element);
+void set_matrix_element(Matrix *matrix, size_t line, size_t column,
+                        double element);
 
 /**
  * @brief Multiplies two matrices and stores the result in a third matrix.
- * 
+ *
  * @param m1 The first matrix to multiply.
  * @param m2 The second matrix to multiply.
  * @param result The matrix in which to store the result of the multiplication.
@@ -79,7 +83,7 @@ void matrix_dot_multiply(Matrix *m1, Matrix *m2, Matrix *result);
 
 /**
  * @brief Multiplies two matrices and stores the result in a third matrix.
- * 
+ *
  * @param m1 The first matrix to multiply.
  * @param m2 The second matrix to multiply.
  * @param result The matrix in which to store the result of the multiplication.
@@ -87,8 +91,9 @@ void matrix_dot_multiply(Matrix *m1, Matrix *m2, Matrix *result);
 void matrix_multiply(Matrix *m1, Matrix *m2, Matrix *result);
 
 /**
- * @brief Multiplies a matrix by a double-precision factor and stores the result in another matrix.
- * 
+ * @brief Multiplies a matrix by a double-precision factor and stores the result
+ * in another matrix.
+ *
  * @param m1 The matrix to multiply.
  * @param factor The multiplication factor.
  * @param result The matrix in which to store the result of the multiplication.
@@ -97,7 +102,7 @@ void matrix_multiply_by_double(Matrix *m1, double factor, Matrix *result);
 
 /**
  * @brief Adds two matrices and stores the result in a third matrix.
- * 
+ *
  * @param m1 The first matrix to add.
  * @param m2 The second matrix to add.
  * @param result The matrix in which to store the result of the addition.
@@ -106,7 +111,7 @@ void matrix_add(Matrix *m1, Matrix *m2, Matrix *result);
 
 /**
  * @brief Subtracts two matrices and stores the result in a third matrix.
- * 
+ *
  * @param m1 The first matrix to subtract.
  * @param m2 The second matrix to subtract.
  * @param result The matrix in which to store the result of the subtraction.
@@ -115,7 +120,7 @@ void matrix_subtract(Matrix *m1, Matrix *m2, Matrix *result);
 
 /**
  * @brief Transposes a matrix and stores the result in another matrix.
- * 
+ *
  * @param m The matrix to transpose.
  * @param result The matrix in which to store the result of the transposition.
  */
@@ -123,18 +128,24 @@ void matrix_transpose(Matrix *m, Matrix *result);
 
 /**
  * @brief Copies the contents of one matrix into another matrix.
- * 
+ *
  * @param m1 The source matrix.
  * @param m2 The destination matrix.
  */
 void matrix_copy(Matrix *m1, Matrix *m2);
 
 /**
+ * @brief Set the identity matrix to a given Matrix
+ *
+ * @param m The source matrix.
+ */
+void set_identity_matrix(Matrix *m);
+
+/**
  * @brief Releases the memory allocated for a matrix.
- * 
+ *
  * @param matrix The matrix to destroy.
  */
 void matrix_destructor(Matrix *matrix);
 
-
-#endif //MATRIX_H
+#endif // MATRIX_H
